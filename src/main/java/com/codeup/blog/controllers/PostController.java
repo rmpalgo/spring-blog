8485735d23ec65a9f2b9b6edbd13e220aa05b2ae
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-29 13:45:42 -0500

    Feat: showed email of post user on the show page.

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -3,6 +3,7 @@ package com.codeup.blog.controllers;
 import com.codeup.blog.daos.BlogsRepository;
 import com.codeup.blog.daos.UsersRepository;
 import com.codeup.blog.models.Post;
+import com.codeup.blog.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -66,8 +67,11 @@ public class PostController {
     @PostMapping("/posts/create")
     public String save(Model model,
                        @RequestParam(name = "title") String title,
-                       @RequestParam(name = "body") String body) {
-        Post newPost = new Post(title, body, null);
+                       @RequestParam(name = "body") String body,
+                       @RequestParam(name = "user") Long user) {
+        Long userId = user;
+        User userObj = usersDao.getOne(userId);
+        Post newPost = new Post(title, body, userObj);
         postsDao.save(newPost);
         List<Post> posts = postsDao.findAll();
         model.addAttribute("posts", posts);
