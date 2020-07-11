9f8693383acdb0181b8197014628832218bf96d0
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-07-11 15:48:42 -0500

    Feat: created model for comments.

---
+++ src/main/java/com/codeup/blog/models/Comment.java
@@ -0,0 +1,80 @@
+package com.codeup.blog.models;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String body;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Post post;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column()
+    private Date createdAt;
+
+    public Comment() {}
+
+    public Comment(long id, String body, User user, Post post, Date createdAt) {
+        this.id = id;
+        this.body = body;
+        this.user = user;
+        this.post = post;
+        this.createdAt = createdAt;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
