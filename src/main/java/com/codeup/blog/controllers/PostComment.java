3286f6260d04b7b29e93842bae0dd271ff945a48
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-07-11 18:25:10 -0500

    Feat: submitting comments work and can be rendered on separate comment page.

---
+++ src/main/java/com/codeup/blog/controllers/PostComment.java
@@ -0,0 +1,57 @@
+package com.codeup.blog.controllers;
+
+import com.codeup.blog.daos.BlogsRepository;
+import com.codeup.blog.daos.CommentsRepository;
+import com.codeup.blog.daos.UsersRepository;
+import com.codeup.blog.models.Comment;
+import com.codeup.blog.models.Post;
+import com.codeup.blog.models.User;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+public class PostComment {
+
+    private BlogsRepository postsDao;
+    private UsersRepository usersDao;
+    private CommentsRepository commentsDao;
+
+    public PostComment(BlogsRepository blogsRepository, UsersRepository usersRepository, CommentsRepository commentsRepository) {
+        this.postsDao = blogsRepository;
+        this.usersDao = usersRepository;
+        this.commentsDao = commentsRepository;
+    }
+
+    @PostMapping("/comments/create")
+    public String createComment( @ModelAttribute Comment commentToBeSaved, Principal principal) {
+        Post post = postsDao.getOne(1L);
+        User user = usersDao.getOne(1L);
+        commentToBeSaved.setPost(post);
+        commentToBeSaved.setUser(user);
+        Date currentDate = new Date();
+        commentToBeSaved.setCreatedAt(currentDate);
+        commentsDao.save(commentToBeSaved);
+        return "redirect:/comments";
+    }
+
+
+    @GetMapping("/comments/create")
+    public String showCommentForm(Model viewModel) {
+        viewModel.addAttribute("comment", new Comment());
+        return "posts/comments";
+    }
+
+    @GetMapping("/comments")
+    public String showAll(Model model) {
+        List<Comment> comments = commentsDao.findAll();
+        model.addAttribute("comments", comments);
+        return "posts/viewComments";
+    }
+
+}
